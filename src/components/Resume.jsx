8f7f3f7020f22f88a2752318dd779b52243a947b
ENSUU <eunsu.dk.kim@gmail.com>
2023-12-13 18:48:30 -0500

    Apply basic layout of how generated resume will be displayed to user
    Set the general layout of the resume and how the information provided
    by the user will be laid out on the webpage.

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,15 +1,24 @@
+import '../styles/resume.css';
+
 function Resume() {
     return (
         <div className="userResume">
-            <h6>(First Name) (Last Name)</h6>
+            <h6 className="userName">(First Name) (Last Name)</h6>
             <div className="userInfo">
-                <h6>(City)</h6>
-                <h6>(Email)</h6>
-                <h6>(LinkedIn)</h6>
-                <h6>(GitHub)</h6>
+                <h6>(City) | (Email) | (LinkedIn) | (GitHub)</h6>
             </div>
             <div className="userEducation">
                 <h6>Education</h6>
+                <hr />
+                <p>(College Name) | (College Location) <span className="gradDate">(Grad Date)</span></p>
+                <p><i>(Some Degree in Some Major)</i> <span className="GPA">(Some GPA)</span></p>
+                <p><b>Relevant Coursework</b>: (List coursework here)</p>
+            </div>
+            <div className="userExperience">
+                <h6>Experience</h6>
+                <hr />
+                <p>(Company Name) - (Position Title) | (Location) <span className='employmentDates'>(Start Date) - (End Date)</span></p>
+                <p>(Make a list here, iterating through each sentence of inputted description)</p>
             </div>
         </div>
     )
