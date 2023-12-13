a3745a0f47a64e08aba6a2672d3e900baacac0eb
ENSUU <eunsu.dk.kim@gmail.com>
2023-12-13 15:53:51 -0500

    Create the basic General component
    Created React component where the user will add their general
    information like name, email, and phone number.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from './components/Navbar';
+import General from './components/General'; 
 
 function App() {
   return (
     <>
       <Navbar />
+      <General />
     </>
   )
 }
---
+++ src/components/General.jsx
@@ -0,0 +1,17 @@
+function General() {
+    return (
+        <form>
+            {/* User's Name Field */}
+            <label htmlFor="">Name</label>
+            <input id="userName" name="userName" type="text"/>
+            {/* User's Email Field */}
+            <label htmlFor="">Email</label>
+            <input id="userEmail" name="userEmail" type="email"/>
+            {/* User's Phone Number */}
+            <label htmlFor="">Phone Number</label>
+            <input id="userPhone" name="userPhone" type="number"/>
+        </form>
+    )
+}
+
+export default General; 
