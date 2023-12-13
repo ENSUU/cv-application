bc8969821046bdcddfe4abad84df2d7a1612f8e3
ENSUU <eunsu.dk.kim@gmail.com>
2023-12-13 16:01:39 -0500

    Add the basic Education components
    Created/added React component where the user will add their
    educational experience.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from './components/Navbar';
 import General from './components/General'; 
+import Education from './components/Education';
 
 function App() {
   return (
     <>
       <Navbar />
       <General />
+      <Education />
     </>
   )
 }
---
+++ src/components/Education.jsx
@@ -0,0 +1,20 @@
+function Education() {
+    return (
+        <form>
+            {/* User's School Field */}
+            <label htmlFor="">School</label>
+            <input id="userSchool" name="userSchool" type="text"/>
+            {/* User's Degree Field */}
+            <label htmlFor="">Degree</label>
+            <input id="userDegree" name="userMajor" type="email"/>
+            {/* User's Education Start Date */}
+            <label htmlFor="">Start Date</label>
+            <input type="text" id="userStartDate" name="userStartDate"/>
+            {/* User's Education End Date */}
+            <label htmlFor="">End Date</label>
+            <input type="text" id="userEndDate" name="userEndDate"/>
+        </form>
+    )
+}
+
+export default Education; 
