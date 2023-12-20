4735f463cb86ef427eaca8dfe253e4d7d857c775
ENSUU <eunsu.dk.kim@gmail.com>
2023-12-20 16:45:09 -0500

    Remove GitHub username from the Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import '../styles/navbar.css';
 function Navbar() {
     return (
         <nav>
-            <h1><i>ENSUU's CV Application</i></h1>
+            <h1><i>CV Application</i></h1>
         </nav>
     )
 }
