48b44197963d03e6b9cad68ece2a9a073e0d5095
ENSUU <eunsu.dk.kim@gmail.com>
2023-12-21 17:55:12 -0500

    Conditionally render the user's entered Experience and Education
    The resume displayed to the user will only show the Education and
    Experience sections when the user saves at least one entry in the
    respective section.

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -2,28 +2,36 @@ import ResumeHeader from './ResumeHeader';
 import ResumeEducation from './ResumeEducation';
 import ResumeExperience from './ResumeExperience';
 
+// app.css is also being applied to this component. 
 import '../styles/resume.css';
-// userSchool, userDegree, userGPA, userGradDate, userCourses, 
-// userCompany, userPosition, userStartDate, userEndDate, positionLocation, positionDescription 
+
 function Resume({ userName, userMail, userCity, userLinkedIn, userGitHub, userEducationList, userExperienceList}) {
     return ( 
         <div className="userResume">
             <ResumeHeader userName={userName} userMail={userMail} userCity={userCity} userLinkedIn={userLinkedIn} userGitHub={userGitHub} />
-            <div className="userEducation">
-                <h4>Education</h4>
-                <hr />
-                {userEducationList.map(education => {
-                    console.log(education);
-                    return <ResumeEducation key={education.userSchool} education={education} />
-                })}
-            </div>
-            <div className="userExperience">
-                <h4>Experience</h4>
-                <hr />
-                {userExperienceList.map(experience => {
-                    return <ResumeExperience key={experience.userCompany + experience.userStartDate} userExperience={experience} />
-                })}
-            </div>
+            {/* Conditionally render the Education section of the user's resume. Only after user adds at least one Education entry.  */}
+            {userEducationList.length !== 0 && (
+                <div className="userEducation">
+                    <h4>Education</h4>
+                    <hr />
+                    {userEducationList.map(education => {
+                        console.log(education);
+                        return <ResumeEducation key={education.userSchool} education={education} />
+                    })}
+                </div>
+              )
+            }
+            {/* Conditionally render the Experience section of the user's resume. Only after user adds at least one Experience entry.  */}
+            {userExperienceList.length !== 0 && (
+                <div className="userExperience">
+                    <h4>Experience</h4>
+                    <hr />
+                    {userExperienceList.map(experience => {
+                        return <ResumeExperience key={experience.userCompany + experience.userStartDate} userExperience={experience} />
+                    })}
+                </div>
+              )
+            }
         </div>
     )
 }
